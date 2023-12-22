6123dace6db66521d528b3df09dc5361fe20afd9
ASHOK <ashokkumar.padala06@gmail.com>
2023-12-22 21:00:43 +0530

    assignments

---
+++ src/main/scala/scala_30_problems.scala
@@ -0,0 +1,116 @@
+case object scala_30_problems {
+  def main(args: Array[String]): Unit = {
+    var x = 0
+    while (x<1){
+
+      if (x>10 && x<40) {
+        println("ashok")}
+      else if (x<5){
+        println("padala")}
+      else {
+        println("kumar")}
+      x+=1
+
+
+    }
+  }
+
+
+  kilo()
+  def kilo(): Unit ={
+    var kgs= 56
+    println(kgs*1000 ,"grams")
+  }
+  franht_to_cel()
+  def franht_to_cel(): Unit ={
+    var c = 80
+    var f = (c*9/5)+32
+    println(f)
+  }
+  highest()
+  def highest(): Unit={
+    var x = scala.io.StdIn.readInt
+    var y = 80
+    var z = 50
+    if (x>y & x>z){println(x)}
+    else if (y>x & y>z) {println(y)}
+    else {println(z)}
+  }
+  /*4. Write a java program that performs the following tasks.
+a. Store a number in a variable
+b. If value is not in range (100-1000) prints wrong number else follows
+the steps
+c. Check even or odd
+d. If even divide the number by 3 and print the remainder
+e. If odd divide the number by 2 and print the remainder.
+*/
+  java_p()
+  def java_p(): Unit = {
+    var num = 15
+    if (!(1 to 1000 contains num ) ) {
+      println("wrong number")}
+    else if (num%2==0){
+      println(num%3)}
+    else  {println(num%2)}
+
+  }
+  /*5. Declare & initialize a number. Check whether the number is in range 0-100
+  or not. If not in range print invalid input. Else – if the number is in range 90-
+    100 then print Super Smart, 80-90 print Smart,70-80 print smart enough,
+  60-70 print just smart, 35-60 print no smart, 0-35 print dump.*/
+  five()
+  def five():Unit ={
+    var num1  = scala.io.StdIn.readInt
+    if (!(1 to 100 contains num1)) {
+      println("invalid input")}
+    else if (90 to 100  contains num1 ){
+      println("super smart")}
+    else if (80 to 90  contains num1 ){
+      println("smart  ")}
+    else if (70 to 80  contains num1 ){
+      println("smart enough ")}
+    else if (60 to 70  contains num1 ){
+      println("just smart")}
+    else if (35 to 60  contains num1 ){
+      println("no  smart")}
+    else if (0 to 35  contains num1 ){
+      println("dump")}
+
+
+  }
+  seeko()
+  def seeko() :Unit = {
+    for (i <- 1 to 60){
+      println("SEEKHO BIGDATA")
+    }
+  }
+  DIV_BY_11()
+  def DIV_BY_11(): Unit = {
+    var a = 250
+    while (a<=550){
+      if (a %11==0){
+        println(a)}
+      a+=1
+
+    }
+
+  }
+  sum_of()
+  def sum_of():Unit = {
+    var sum_of_num = 0
+    for(i<- 56 to 153){
+
+      sum_of_num += i
+    }
+    println(sum_of_num)
+  }
+
+
+
+
+}
+
+
+
+
+
